6d033a1756b1b11e08141886467f298d2a40a619
Christopher Holt <holt.christopher1@gmail.com>
2022-08-09 16:38:56 +1000

    adding some information to the migration tables

--- burning-server/db/migrate/20220809062004_create_airplanes.rb
+++ burning-server/db/migrate/20220809062004_create_airplanes.rb
@@ -1,6 +1,11 @@
 class CreateAirplanes < ActiveRecord::Migration[5.2]
   def change
     create_table :airplanes do |t|
+      t.text :name
+      t.text :rows
+      t.text :column
+
+      t.timestamps
     end
   end
 end
