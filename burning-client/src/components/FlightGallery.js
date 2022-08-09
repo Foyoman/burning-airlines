0d532d486cfa60e416c91431e155215b5348e256
Edward Do <edwardtdo@gmail.com>
2022-08-09 20:31:39 +1000

    added lodash to client

--- burning-client/src/components/FlightGallery.js
+++ burning-client/src/components/FlightGallery.js
@@ -3,8 +3,29 @@ import React, { useState } from 'react';
 const FlightGallery = (props) => {
     return (
         <div>
-            <p>{ props.flights.length } flights</p>
-            { props.flights.map((f) => <p key={ f.id }>{ f.flight_number }</p>) }
+            <h2>{ props.flights.length } Burning Flights</h2>
+            <table>
+                <thead>
+                    <tr>
+                    <th>Flight number</th>
+                    <th>Origin</th>
+                    <th>Destination</th>
+                    <th>Date</th>
+                    <th>Airplane</th>
+                    <th colspan="3"></th>
+                    </tr>
+                </thead>
+            
+            
+                <tbody>
+                    {/* { props.flights.forEach } */}
+
+                    { props.flights.map((f) => <p key={ f.id }>{ f.flight_number }</p>) }
+                    
+
+                </tbody>
+
+            </table>
         </div>
     )
 }
