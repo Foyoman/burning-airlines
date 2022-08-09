022be39e3493236c7334e2ed4fb48cc75daeac80
Christopher Holt <holt.christopher1@gmail.com>
2022-08-09 20:33:35 +1000

    Adding some flight search functionality

--- burning-client/src/components/App.js
+++ burning-client/src/components/App.js
@@ -1,9 +1,11 @@
 import BurningAirlines from './BurningAirlines';
+import FlightSearch from './FlightSearch';
 
 function App() {
   return (
     <div className="App">
       <BurningAirlines />
+      <FlightSearch />
     </div>
   );
 }
