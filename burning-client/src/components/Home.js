3d71b7a69f14ece0ff85b9fb0a147e3aa289186f
Christopher Holt <holt.christopher1@gmail.com>
2022-08-11 00:01:03 +1000

    adding password to login

--- burning-client/src/components/Home.js
+++ burning-client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class Home extends Component {
     _handleLogin(email) {
@@ -19,14 +19,20 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ''
+            email: '',
+            password: ''
         }
-        this._handleChange = this._handleChange.bind(this);
+        this._handleEmail = this._handleEmail.bind(this);
+        this._handlePassword = this._handlePassword.bind(this);
         this._handleSubmit = this._handleSubmit.bind(this);
     }
 
-    _handleChange(event) {
+    _handleEmail(event) {
         this.setState({ email: event.target.value });
+    }
+
+    _handlePassword(event) {
+        this.setState({ password: event.target.value });
     }
 
     _handleSubmit(event) {
@@ -34,25 +40,28 @@ class Login extends Component {
         this.props.onSubmit(this.state.email);
         this.setState({ email: '' });
 
-        const SERVER_URL = 'http://localhost:3001/flights';
+        const SERVER_URL = 'http://localhost:3001/search';
 
-        if (this.state.email === 'CJ.Adlay@gmail.com')
+        if (this.state.email === 'CJ.Adlay@gmail.com' && this.state.password === 'chicken')
         {
             console.log('Login succesful');
             window.location.href = `${ SERVER_URL }`;
+        } else {
+            console.log('Login failed');
         }
     }
 
     render() {
         return (
-            <div class="Login">
+              <div class="Login">
                 <h2>Login</h2>
                 <form onSubmit={ this._handleSubmit }>
-                    <input onChange={ this._handleChange } value={ this.state.email } type="text" placeholder="Email" email="email" />
+                    <input onChange={ this._handleEmail } value={ this.state.email } type="text" placeholder="Email" email="email" />
+                    <input onChange={ this._handlePassword } value={ this.state.password } type="text" placeholder="Password" password="password" />
                     <input type="submit" value="Login" />
                 </form>
-            </div>
-        );
+            </div>  
+        )
     }
 }
 
--- burning-server/db/seeds.rb
+++ burning-server/db/seeds.rb
@@ -1,7 +1,9 @@
 User.destroy_all
 u1 = User.create :email => 'EddieGrugrer@gmail.com', :password => 'chicken', :admin => true 
 u2 = User.create :email => 'GuillyWilly@gmail.com', :password => 'chicken'
-u3 = User.create :email =>'CJ.Adlay@gmail.com', :password  => 'chicken', :admin => true
+u3 = User.create :email => 'CJ.Adlay@gmail.com', :password  => 'chicken', :admin => true
+u4 = User.create :email => 'admin.ga.co', :password => 'chicken', :admin => true
+puts "#{ User.count } users"
 
 Airplane.destroy_all
 a1 = Airplane.create :name => 'BF 109', :rows => 15, :columns => 4
@@ -10,14 +12,6 @@ a3 = Airplane.create :name => 'David Boeing 777', :rows => 30, :columns => 5
 a4 = Airplane.create :name => 'Bang Airbus 69', :rows => 30, :columns => 6
 a5 = Airplane.create :name => 'Osama Was An Actor', :rows => 20, :columns => 5
 puts "#{ Airplane.count } airplanes"
-
-User.destroy_all
-u1 = User.create :email => 'EddieGrugrer@gmail.com', :password => 'chicken', :admin => true 
-u2 = User.create :email => 'GuillyWilly@gmail.com', :password => 'chicken'
-u3 = User.create :email => 'CJ.Adlay@gmail.com', :password  =>'chicken' 
-u4 = User.create :email => 'TenzangMyBoo@gmail.com', :password =>'chicken'
-u5 = User.create :email => 'JoeTurnsBull@gmail.com', :password => 'chicken', :admin => true 
-puts "#{ User.count } users"
 
 Flight.destroy_all 
 f1 = Flight.create :flight_number => 'E26', :origin => 'MEL', :destination => 'SUD', :date => Time.new
